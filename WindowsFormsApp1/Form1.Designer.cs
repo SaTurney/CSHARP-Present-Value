b4e2988ac67c2e48d68cb3ce31661c3293768fde
Sabrina Turney <turney.sabrina@gmail.com>
2020-01-03 20:53:28 -0500

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class presentValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calcPresentValue = new System.Windows.Forms.Button();
+            this.exitProgram = new System.Windows.Forms.Button();
+            this.introLabel = new System.Windows.Forms.Label();
+            this.presentValueText = new System.Windows.Forms.Label();
+            this.calculatedPresentValue = new System.Windows.Forms.TextBox();
+            this.futureLabel = new System.Windows.Forms.Label();
+            this.annualInterestLabel = new System.Windows.Forms.Label();
+            this.yearsLabel = new System.Windows.Forms.Label();
+            this.futureValue = new System.Windows.Forms.TextBox();
+            this.annualInterestRate = new System.Windows.Forms.TextBox();
+            this.yearsFermenting = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // calcPresentValue
+            // 
+            this.calcPresentValue.Location = new System.Drawing.Point(117, 412);
+            this.calcPresentValue.Name = "calcPresentValue";
+            this.calcPresentValue.Size = new System.Drawing.Size(247, 89);
+            this.calcPresentValue.TabIndex = 0;
+            this.calcPresentValue.Text = "Calculate Present Value";
+            this.calcPresentValue.UseVisualStyleBackColor = true;
+            this.calcPresentValue.Click += new System.EventHandler(this.CalcPresentValue_Click);
+            // 
+            // exitProgram
+            // 
+            this.exitProgram.Location = new System.Drawing.Point(471, 412);
+            this.exitProgram.Name = "exitProgram";
+            this.exitProgram.Size = new System.Drawing.Size(244, 86);
+            this.exitProgram.TabIndex = 1;
+            this.exitProgram.Text = "Exit";
+            this.exitProgram.UseVisualStyleBackColor = true;
+            this.exitProgram.Click += new System.EventHandler(this.ExitProgram_Click);
+            // 
+            // introLabel
+            // 
+            this.introLabel.AutoSize = true;
+            this.introLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.introLabel.Location = new System.Drawing.Point(238, 23);
+            this.introLabel.Name = "introLabel";
+            this.introLabel.Size = new System.Drawing.Size(424, 60);
+            this.introLabel.TabIndex = 2;
+            this.introLabel.Text = "Caclulate the \"Present Value\" of your investment,\r\nthe amount you should deposit " +
+    "today,\r\nby inputting values into the following boxes: ";
+            this.introLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // presentValueText
+            // 
+            this.presentValueText.AutoSize = true;
+            this.presentValueText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.presentValueText.Location = new System.Drawing.Point(57, 347);
+            this.presentValueText.Name = "presentValueText";
+            this.presentValueText.Size = new System.Drawing.Size(394, 24);
+            this.presentValueText.TabIndex = 3;
+            this.presentValueText.Text = "The Present Value for your investment is:";
+            // 
+            // calculatedPresentValue
+            // 
+            this.calculatedPresentValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.calculatedPresentValue.Location = new System.Drawing.Point(487, 349);
+            this.calculatedPresentValue.Name = "calculatedPresentValue";
+            this.calculatedPresentValue.ReadOnly = true;
+            this.calculatedPresentValue.Size = new System.Drawing.Size(228, 22);
+            this.calculatedPresentValue.TabIndex = 4;
+            // 
+            // futureLabel
+            // 
+            this.futureLabel.AutoSize = true;
+            this.futureLabel.Location = new System.Drawing.Point(114, 144);
+            this.futureLabel.Name = "futureLabel";
+            this.futureLabel.Size = new System.Drawing.Size(330, 17);
+            this.futureLabel.TabIndex = 5;
+            this.futureLabel.Text = "Enter the future value that you want in the account:";
+            // 
+            // annualInterestLabel
+            // 
+            this.annualInterestLabel.AutoSize = true;
+            this.annualInterestLabel.Location = new System.Drawing.Point(129, 212);
+            this.annualInterestLabel.Name = "annualInterestLabel";
+            this.annualInterestLabel.Size = new System.Drawing.Size(315, 17);
+            this.annualInterestLabel.TabIndex = 6;
+            this.annualInterestLabel.Text = "Enter the the annual interest rate of the account:";
+            // 
+            // yearsLabel
+            // 
+            this.yearsLabel.AutoSize = true;
+            this.yearsLabel.Location = new System.Drawing.Point(129, 264);
+            this.yearsLabel.Name = "yearsLabel";
+            this.yearsLabel.Size = new System.Drawing.Size(322, 34);
+            this.yearsLabel.TabIndex = 7;
+            this.yearsLabel.Text = "Enter the fthe number of years that you plan to let\r\nthe money sit in the account" +
+    ":";
+            // 
+            // futureValue
+            // 
+            this.futureValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.futureValue.Location = new System.Drawing.Point(487, 146);
+            this.futureValue.Name = "futureValue";
+            this.futureValue.Size = new System.Drawing.Size(228, 22);
+            this.futureValue.TabIndex = 8;
+            // 
+            // annualInterestRate
+            // 
+            this.annualInterestRate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.annualInterestRate.Location = new System.Drawing.Point(487, 207);
+            this.annualInterestRate.Name = "annualInterestRate";
+            this.annualInterestRate.Size = new System.Drawing.Size(228, 22);
+            this.annualInterestRate.TabIndex = 11;
+            // 
+            // yearsFermenting
+            // 
+            this.yearsFermenting.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.yearsFermenting.Location = new System.Drawing.Point(487, 276);
+            this.yearsFermenting.Name = "yearsFermenting";
+            this.yearsFermenting.Size = new System.Drawing.Size(228, 22);
+            this.yearsFermenting.TabIndex = 12;
+            // 
+            // presentValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 541);
+            this.Controls.Add(this.yearsFermenting);
+            this.Controls.Add(this.annualInterestRate);
+            this.Controls.Add(this.futureValue);
+            this.Controls.Add(this.yearsLabel);
+            this.Controls.Add(this.annualInterestLabel);
+            this.Controls.Add(this.futureLabel);
+            this.Controls.Add(this.calculatedPresentValue);
+            this.Controls.Add(this.presentValueText);
+            this.Controls.Add(this.introLabel);
+            this.Controls.Add(this.exitProgram);
+            this.Controls.Add(this.calcPresentValue);
+            this.Name = "presentValue";
+            this.Text = "Present Value";
+            this.Load += new System.EventHandler(this.PresentValue_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcPresentValue;
+        private System.Windows.Forms.Button exitProgram;
+        private System.Windows.Forms.Label introLabel;
+        private System.Windows.Forms.Label presentValueText;
+        private System.Windows.Forms.TextBox calculatedPresentValue;
+        private System.Windows.Forms.Label futureLabel;
+        private System.Windows.Forms.Label annualInterestLabel;
+        private System.Windows.Forms.Label yearsLabel;
+        private System.Windows.Forms.TextBox futureValue;
+        private System.Windows.Forms.TextBox annualInterestRate;
+        private System.Windows.Forms.TextBox yearsFermenting;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,122 @@
+﻿//Sabrina Turney
+//C# I
+//Program 6-7: Present Value
+//Last Edited 9/14/2019
+
+/*
+Program Requirements and Notes:
+Suppose you want to deposit a certain amount of money into a savings account and then leave it alone to draw interest for the next 10 years. 
+At the end of 10 years you would like to have $10,000 in the account. How much do you need to deposit today to make that happen? 
+You can use the following formula, which is known as the present-value formula, to find out:
+
+       F
+P =  -------
+    (1+r)^n
+
+The terms in the formula are as follows:
+P is the present value, or the amount that you need to deposit today.
+F is the future value that you want in the account. (In this case, F is $10,000.)
+r is the annual interest rate.
+n is the number of years that you plan to let the money sit in the account.
+
+Write a method named PresentValue that performs this calculation. The method should accept the future value, annual interest rate, 
+and number of years as arguments. It should return the present value, which is the amount that you need to deposit today. 
+Demonstrate the method in an application that lets the user experiment with different values for the formula’s terms. 
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+
+    public partial class presentValue : Form
+    {
+
+        public presentValue()
+        {
+            InitializeComponent();
+        }
+
+
+        private void PresentValue_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // The InputIsValid method converts the user input and stores 
+        // it in the arguments (passed by reference). If the conversion 
+        // is successful, the method returns true. Otherwise it returns 
+        // false. 
+
+        
+          //This section of code is taken from the tutorial and tweaked so it can validate all three entered values by the user.
+
+        private bool InputIsValid(ref decimal userFutureValue, ref decimal userAnnualInterest, ref decimal userYears)
+        {
+            // Flag variable to indicate whether the input is good 
+            bool inputGood = false;
+
+            // Try to convert all inputs to decimals. 
+
+            if (decimal.TryParse(futureValue.Text, out userFutureValue))
+            {
+                if (decimal.TryParse(annualInterestRate.Text, out userAnnualInterest))
+                {
+                    if (decimal.TryParse(yearsFermenting.Text, out userYears))
+                    {
+                        // ALL inputs are good. 
+                        inputGood = true;
+                    }
+                }
+
+            }
+
+            else
+            { // Display an error message for the input. I refactored the tutorial code here- The user will understand what the input needs to be, instead of just receiving an error. 
+                MessageBox.Show("One of your inputs is invalid. Please enter decimals or whole numbers only.");
+            }
+            // Return the result. 
+            return inputGood;
+        }
+        private void CalcPresentValue_Click(object sender, EventArgs e)
+        {
+            //First I start off by declaring all the variables I'll use to calculate.
+            decimal presentValue = 0m;
+            decimal userFutureValue = 0m;
+            decimal userAnnualInterest = 0m;
+            decimal userYears = 0m;
+
+
+            //only complete the calculations if the user inputs the correct values.
+            if (InputIsValid(ref  userFutureValue, ref  userAnnualInterest, ref userYears))
+            {
+                //retrieve the values from user-entered information.
+                userFutureValue = decimal.Parse(futureValue.Text);
+                userAnnualInterest = decimal.Parse(annualInterestRate.Text);
+                userYears = decimal.Parse(yearsFermenting.Text);
+
+                //complete calculations, display result in text box for user.
+                presentValue = userFutureValue / (decimal)Math.Pow((double)(1 + userAnnualInterest), (double)userYears);
+                calculatedPresentValue.Text = presentValue.ToString("c");
+            }
+
+
+        }
+
+        private void ExitProgram_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
